8737381f74d7ca1ab4bd868383b0b0830866535b
Unknown <tom@dlnet.us>
2019-01-02 04:51:39 +0100

    almost finished

---
+++ Blox Saber Editor/TextureManager.cs
@@ -0,0 +1,70 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Blox_Saber_Editor
+{
+    static class TextureManager
+    {
+        private static readonly Dictionary<string, int> Textures = new Dictionary<string, int>();
+
+        public static int GetOrRegister(string textureName, Bitmap bmp = null)
+        {
+            if (Textures.TryGetValue(textureName, out var texId))
+                return texId;
+
+            Bitmap img = bmp;
+
+            if (img == null)
+            {
+                var file = "assets\\textures\\" + textureName + ".png";
+
+                if (!File.Exists(file))
+                {
+                    Console.WriteLine($"Could not find file {file}");
+                    return -1;
+                }
+
+                using (var fs = File.OpenRead(file))
+                {
+                    img = (Bitmap)Image.FromStream(fs);
+                }
+            }
+
+            var id = LoadTexture(img);
+
+            Textures.Add(textureName, id);
+
+            return id;
+        }
+
+        private static int LoadTexture(Bitmap img)
+        {
+            var id = GL.GenTexture();
+
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            BitmapData data = img.LockBits(new Rectangle(0, 0, img.Width, img.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            img.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
+                (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
+                (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS,
+                (int)TextureWrapMode.ClampToEdge);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT,
+                (int)TextureWrapMode.ClampToEdge);
+
+            return id;
+        }
+    }
+}
